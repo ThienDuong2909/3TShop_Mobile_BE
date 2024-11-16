8a83ab3fcab1efaab0d894661bf38eba65ff8758
Thien <thienplpp965@gmail.com>
2024-11-16 20:48:56 +0700

    fix: login with GG

--- src/main/java/com/project/_TShop/Security/GoogleOAuth2SuccessHandler.java
+++ src/main/java/com/project/_TShop/Security/GoogleOAuth2SuccessHandler.java
@@ -2,8 +2,11 @@ package com.project._TShop.Security;
 
 import com.project._TShop.Entities.Account;
 import com.project._TShop.Entities.Auth_Provider;
+import com.project._TShop.Entities.Cart;
 import com.project._TShop.Entities.Role;
+import com.project._TShop.Exceptions.ResourceNotFoundException;
 import com.project._TShop.Repositories.AccountRepository;
+import com.project._TShop.Repositories.CartRepository;
 import com.project._TShop.Repositories.RoleRepository;
 import com.project._TShop.Services.JwtService;
 import jakarta.servlet.http.HttpServletRequest;
@@ -25,32 +28,32 @@ public class GoogleOAuth2SuccessHandler implements AuthenticationSuccessHandler 
     private final AccountRepository accountRepo;
     private final JwtService jwtService;
     private final RoleRepository roleRepo;
+    private final CartRepository cartRepository;
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, IOException {
         OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
         String email = oAuth2User.getAttribute("email");
-//        String Username = oAuth2User.getAttribute("name")
+        String Username = oAuth2User.getAttribute("name");
         Account account = accountRepo.findByEmail(email).orElseGet(() -> {
             Collection<Role> roles = new ArrayList<>();
             Optional<Role> optionalRole = roleRepo.findByName("USER");
-
             roles.add(optionalRole.get());
-
             Account newAccount = new Account();
             newAccount.setEmail(email);
-            newAccount.setUsername(email);
+            newAccount.setUsername(Username);
             newAccount.setAuth_provider(String.valueOf(Auth_Provider.GOOGLE));
             newAccount.setStatus(true);
             newAccount.setCreatedAt(new Date());
             newAccount.setRoles(roles);
             return accountRepo.save(newAccount);
         });
-
+        cartRepository.findByAccount(account)
+                .orElseGet(() -> {
+                    Cart newCart = new Cart();
+                    newCart.setAccount(account);
+                    return cartRepository.save(newCart);
+                });
         String token = jwtService.generateToken(account);
-//        ResponseEntity<String> responseEntity = ResponseEntity.status(200).body(token);
-//
-//        response.setContentType("application/json");
-//        response.getWriter().write(Objects.requireNonNull(responseEntity.getBody()));
         response.sendRedirect("http://localhost:3003/login?token=" + token);
     }
 }
